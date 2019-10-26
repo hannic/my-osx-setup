5c3843359d42c14e3a6700315694ba500d565c9a
hannic <nicole.haenni@gmail.com>
2019-10-26 20:32:27 +0200

    Create update-brew.sh

---
+++ helper/update-brew.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+PATH=/usr/local/bin:/usr/bin:/bin
+
+# -----------------------------------------------------------------------------
+# http://onyxmueller.net/2018/08/31/scheduling-commands-with-cron-on-your-mac/
+# usage with cronjob: @daily /path/to/shell/commands.sh
+# -----------------------------------------------------------------------------
+brew cleanup
+brew cask cleanup
+brew update
+
